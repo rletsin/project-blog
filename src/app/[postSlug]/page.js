e871871f3ebd758952bf46974325fbb27a0bed92
Roman Letsin <rletsin25@gmail.com>
2025-09-22 15:58:20 +0300

    Display the list of blogposts with content

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
-
+import { loadBlogPost } from '@/helpers/file-helpers';
 import styles from './postSlug.module.css';
 
-function BlogPost() {
+async function BlogPost({ params }) {
+  const { frontMatter, content } = await loadBlogPost(params.postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
@@ -12,12 +14,7 @@ function BlogPost() {
         publishedOn={new Date()}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
