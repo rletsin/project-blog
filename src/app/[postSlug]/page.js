a9ef06a0e0d9a85b0e9cf6a4436c195825e2cbef
Roman Letsin <rletsin25@gmail.com>
2025-09-22 16:51:22 +0300

    Add metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
+import { BLOG_TITLE } from '@/constants';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import styles from './postSlug.module.css';
 
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+
+  const { frontmatter } = await loadBlogPost(
+    postSlug
+  );
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
+
 async function BlogPost({ params }) {
-  const { frontMatter, content } = await loadBlogPost(params.postSlug);
+  const { postSlug } = await params;
+  const { frontmatter, content } = await loadBlogPost(postSlug);
 
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
         <MDXRemote source={content} />
